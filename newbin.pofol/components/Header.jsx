28621a3859032db900174de5c39b1dd016b7824e
newbin4126 <dkfmal710@gmail.com>
2024-01-24 01:37:11 +0900

    newbin.me
    사이트만들기1일차!!후..

---
+++ newbin.pofol/components/Header.jsx
@@ -0,0 +1,11 @@
+import ThemeToggler from "./ThemeToggler";
+
+export const Header = () => {
+    return (
+        <header>
+            <ThemeToggler />
+        </header>
+    );
+};
+
+export default Header;
